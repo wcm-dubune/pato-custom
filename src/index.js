0d401881061db83dff7e05079f665c5773679a6c
wcm-dubune <wcm.dubune@gmail.com>
2019-06-08 19:39:46 +0900

    first init

---
+++ src/index.js
@@ -0,0 +1,39 @@
+import 'es6-promise/auto'
+import Vue from 'vue';
+// import Vuex from 'vuex'
+
+// import WcmMain from './vue/wcm-main.vue';
+
+// Vue.prototype.$EventBus = new Vue();
+
+// Vue.use(Vuex);
+
+// const store = new Vuex.Store({
+// 	state: {
+// 		openModal: false,
+// 		openMenu: false
+// 	},
+// 	mutations: {
+// 		openModal(state) {
+// 			state.openModal = true;
+// 		},
+// 		closeModal(state) {
+// 			state.openModal = false;
+// 		},
+// 		openMenu(state) {
+// 			state.openMenu = true;
+// 		},
+// 		closeMenu(state) {
+// 			state.openMenu = false;
+// 		},
+// 	}
+// });
+
+new Vue({
+	el: '#app',
+	// store,
+	// components: {
+	// 	WcmMain
+	// }
+});
+export default {};
---
+++ src/vue/main.vue
@@ -0,0 +1,13 @@
+<template>
+	<div>test</div>
+</template>
+
+// <script>
+// import Component from './component';
+// export default {
+// 	name: 'wcm-main',
+// 	components: {
+// 		...Component
+// 	}
+// };
+// </script>
